5679d7d920c2c2bccdb0f95c233b93ace040b3dd
alexander weiss <42820958+artorius611@users.noreply.github.com>
2019-01-09 09:26:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,69 @@
 import os 
 
-
-
-str_ch = 10
-dex_ch = 10
-con_ch = 10
-int_ch = 10
-wis_ch = 10
-cha_ch = 10
-speed = 0
+strength = 10
+dexterity = 10
+constitution = 10
+intelligence = 10
+wisdom = 10
+charisma = 10
 health = 0
+speed = 0
 
 class Halfling :
-    def __init__ (self, frodo, str_ch, dex_ch, con_ch, int_ch, wis_ch, cha_ch, speed, health) :
-        frodo = "true"
-        self.dex_ch = dex_ch + 2
-        self.wis_ch = wis_ch + 2
-        self.str_ch = str_ch - 2
-        self.con_ch = con_ch
-        self.int_ch = int_ch
-        self.cha_ch = cha_ch 
+    def __init__ (self, strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed) :
+        self.dexterity = dexterity + 2
+        self.wisdom = wisdom + 2
+        self.strength = strength - 2
+        self.constitution = constitution
+        self.intelligence = intelligence
+        self.charisma = charisma 
+        self.health = health + 6
         self.speed = speed + 25
-        self.ch_hp = health + 6
+        frodo = "true"
+
+class Bonus_Stat :
+    def __init__ (self, strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed) :
+        self.dexterity = dexterity + 2
+        self.wisdom = wisdom + 2
+        self.strength = strength + 2
+        self.constitution = constitution + 2
+        self.intelligence = intelligence + 2
+        self.charisma = charisma + 2
 
 valid_user_input = "false"
 while valid_user_input == "false" :
-    print ('''
-    What is your Ancestry?
-    
-    ''')
+    print ('\nWhat is your Ancestry?\n')
     print ('1) Halfling')
     print ()
     choice = input('Which Ancestry do you choose? ')
     if choice == '1' :
         valid_user_input = "true"
-        user_race = Halfling(str_ch, dex_ch, con_ch, int_ch, wis_ch, cha_ch, speed, ch_hp)
-        #bonus_user_input == "false"
-        #while bonus_user_input == "false":
-        #    print ('''  
-    #
-    #        What is your bonus stat?
-    #
-     #       ''')
-      #      print ('1) Str')
-       #     print ('2) Con')
-        #    print ('3) Int')
-         #   print ('4) Cha')
-         #   choice = input('Which bonus stat do you choose?')
-         #   if choice == 1 :
-         #      bonus_user_input = "true"
-         #        = user_race.str_ch + 2
-         #   elif choice == 2 :
-         #       bonus_user_input = "true"
-         #       con_ch = user_race.con_ch + 2
-         #   elif choice == 3 :
-         #       bonus_user_input = "true"
-         #       int_ch = user_race.int_ch + 2
-         #   elif choice == 4 :
-         #       bonus_user_input = "true"
-         #       cha_ch = user_race.cha_ch + 2
-         #   else:
-         #       print ('''
-         #   
-         #       You selected something invalid
-         #   
-         #       ''')
+        user_race = Halfling (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed)
+        bonus_user_input = "false"
+        while bonus_user_input == "false":
+            print ('\nWhat is your bonus stat?\n')
+            print ('1) Str')
+            print ('2) Con')
+            print ('3) Int')
+            print ('4) Cha')
+            choice = input('Which bonus stat do you choose?')
+            if choice == 1 :
+               bonus_user_input = "true"
+               bonus_user = Halfling(strength) + Bonus_stat(strength)
+               print ('Your Health is:', user_race.health, '\nYour Speed is: ', user_race.speed, 'ft. \nYour Stats are:\n STR:', bonus_user.strength, '\n Dex', user_race.dexterity, '\n Con', user_race.constitution, '\n Int', user_race.intelligence, '\n Wis', user_race.wisdom, '\n Cha', user_race.charisma)
+            elif choice == 2 :
+                bonus_user_input = "true"
+                bonus_user = Halfling(constitution) + Bonus_stat(con)
+            elif choice == 3 :
+                bonus_user_input = "true"
+                intelligence = user_race.intelligence + 2
+            elif choice == 4 :
+                bonus_user_input = "true"
+                charisma = user_race.charisma + 2
+            else:
+                print ('\nYou selected something invalid\n')
             # the string below clears the screen
-         #   os.system ('cls')        
+            os.system ('cls')        
     else:
         print ('''
         
@@ -77,6 +74,4 @@ while valid_user_input == "false" :
         ''')
         # the string below clears the screen
     os.system ('cls')
-print ('''
-Your health is: ''', user_race.ch_hp, ''' \n Your Speed is:''', user_race.speed, '''
-Your Stats are:\n STR:''', user_race.str_ch, '\n Dex', user_race.dex_ch, '\n Con', user_race.con_ch, '\n Int', user_race.int_ch, '\n Wis', user_race.wis_ch, '\n Cha', user_race.cha_ch)
+print ('Your Health is:', user_race.health, '\nYour Speed is: ', user_race.speed, 'ft. \nYour Stats are:\n STR:', user_race.strength, '\n Dex', user_race.dexterity, '\n Con', user_race.constitution, '\n Int', user_race.intelligence, '\n Wis', user_race.wisdom, '\n Cha', user_race.charisma)
