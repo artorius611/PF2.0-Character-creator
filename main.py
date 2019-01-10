09fbf21c3d7ed63e98c6e2bda52bd99b6514acb4
alexander weiss <42820958+artorius611@users.noreply.github.com>
2019-01-10 15:41:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os 
+from Race import Ancestry
 
 strength = 10
 dexterity = 10
@@ -9,50 +10,6 @@ charisma = 10
 health = 0
 speed = 0
 heritage_bonus = 0
-
-class Halfling :
-    def __init__ (self, strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus) :
-        self.dexterity = dexterity + 2
-        self.wisdom = wisdom + 2
-        self.strength = strength - 2
-        self.constitution = constitution
-        self.intelligence = intelligence
-        self.charisma = charisma 
-        self.health = health + 6
-        self.speed = speed + 25
-        frodo = "true"
-        self.halfling_heritage = {
-            'Gutsy' : 'Your family line is known for keeping a level head and staving off fear when the chips go down, making them wise leaders. If you succeed at a saving throw against an emotion effect, treat your result as a critical success instead of a success.',
-            'Jungle' : 'You live deep in the jungle, and you’ve learned how to use your small size to wriggle through undergrowth, vines, and other obstacles. You can ignore difficult terrain from trees and foliage, whether in a jungle or elsewhere.',
-            'Nomadic' : 'Your ancestors have traveled from place to place for generations. You gain a new language of your choice, and every time you take the Multilingual feat, you gain another new language.',
-            'Twilight' : 'Your ancestors performed many acts under cover of dusk and developed eyesight beyond even the usual keen eyes of halflings. You gain low-light vision.'
-        }
-        print (self.halfling_heritage)
-        heritage_input = "false"
-        while heritage_input == "false":
-            print ('\nWhat is your Heritage?\n')
-            print ('1) Gutsy ')
-            print ('2) Jungle')
-            print ('3) Nomadic')
-            print ('4) Twilight')
-            choice = input('')
-            print (choice)
-            if choice == '1' :
-                heritage_input = "true"
-                self.heritage_bonus = self.halfling_heritage["Gutsy"]
-            elif choice == '2' :
-                heritage_input = "true"
-                self.heritage_bonus = self.halfling_heritage['Jungle']
-            elif choice == '3' :
-                heritage_input = "true"
-                self.heritage_bonus = self.halfling_heritage['Nomadic']
-            elif choice == '4' :
-                heritage_input = "true"
-                self.heritage_bonus = self.halfling_heritage['Twilight']
-        else:
-            print ('\n \nYou selected something invalid\n \n')
-        #clear the screen
-        os.system ('cls')   
 
 class Bonus_Stat :
     def __init__ (self, strength, dexterity, constitution, intelligence, wisdom, charisma) :
@@ -71,7 +28,7 @@ while valid_user_input == "false" :
     choice = input('Which Ancestry do you choose? ')
     if choice == '1' :
         valid_user_input = "true"
-        user_race = Halfling (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus)
+        user_race = Ancestry.Halfling (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus)
         bonus_user_input = "true"
         while bonus_user_input == "true":
             print ('\nWhat is your bonus stat?\n')
