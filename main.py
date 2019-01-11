5d9373e4f6f0f66e7579682745daa38040f79f75
alexander weiss <42820958+artorius611@users.noreply.github.com>
2019-01-10 18:06:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os 
-from Race import Ancestry
+from pieces import Ancestry
 
 strength = 10
 dexterity = 10
@@ -24,7 +24,11 @@ valid_user_input = "false"
 while valid_user_input == "false" :
     print ('\nWhat is your Ancestry?\n')
     print ('1) Halfling')
-    print ()
+    print ('2) Dwarf')
+    print ('3) Elf')
+    print ('4) Gnome')
+    print ('5) Goblin')
+    print ('6) Human')
     choice = input('Which Ancestry do you choose? ')
     if choice == '1' :
         valid_user_input = "true"
@@ -48,6 +52,235 @@ while valid_user_input == "false" :
             elif choice == 3 :
                 bonus_user_input = "false"
                 user_race.intelligence = user_race.intelligence + bonus
+            elif choice == 4 :
+                bonus_user_input = "false"
+                user_race.charisma = user_race.charisma + bonus
+            else:
+                print ('\nYou selected something invalid\n')
+            # the string below clears the screen
+            os.system ('cls')
+    elif choice == '2' :
+        valid_user_input = "true"
+        user_race = Ancestry.Dwarf (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus)
+        bonus_user_input = "true"
+        while bonus_user_input == "true":
+            print ('\nWhat is your bonus stat?\n')
+            print ('1) Str')
+            print ('2) Dex')
+            print ('3) Int')
+            print ('4) Cha')
+            choice = int(input(''))
+            print (choice)
+            bonus = 2
+            if choice == 1 :
+                bonus_user_input = "false"
+                user_race.strength = user_race.strength + bonus
+            elif choice == 2 :
+                bonus_user_input = "false"
+                user_race.dexterity = user_race.dexterity + bonus
+            elif choice == 3 :
+                bonus_user_input = "false"
+                user_race.intelligence = user_race.intelligence + bonus
+            elif choice == 4 :
+                bonus_user_input = "false"
+                user_race.charisma = user_race.charisma + bonus
+            else:
+                print ('\nYou selected something invalid\n')
+            # the string below clears the screen
+            os.system ('cls')
+    elif choice == '3' :
+        valid_user_input = "true"
+        user_race = Ancestry.Elf (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus)
+        bonus_user_input = "true"
+        while bonus_user_input == "true":
+            print ('\nWhat is your bonus stat?\n')
+            print ('1) Str')
+            print ('2) Con')
+            print ('3) Wis')
+            print ('4) Cha')
+            choice = int(input(''))
+            print (choice)
+            bonus = 2
+            if choice == 1 :
+                bonus_user_input = "false"
+                user_race.strength = user_race.strength + bonus
+            elif choice == 2 :
+                bonus_user_input = "false"
+                user_race.constitution = user_race.constitution + bonus
+            elif choice == 3 :
+                bonus_user_input = "false"
+                user_race.wisdom = user_race.wisdom + bonus
+            elif choice == 4 :
+                bonus_user_input = "false"
+                user_race.charisma = user_race.charisma + bonus
+            else:
+                print ('\nYou selected something invalid\n')
+            # the string below clears the screen
+            os.system ('cls')
+    elif choice == '4' :
+        valid_user_input = "true"
+        user_race = Ancestry.Gnome (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus)
+        bonus_user_input = "true"
+        while bonus_user_input == "true":
+            print ('\nWhat is your bonus stat?\n')
+            print ('1) Str')
+            print ('2) Dex')
+            print ('3) Wis')
+            print ('4) Int')
+            choice = int(input(''))
+            print (choice)
+            bonus = 2
+            if choice == 1 :
+                bonus_user_input = "false"
+                user_race.strength = user_race.strength + bonus
+            elif choice == 2 :
+                bonus_user_input = "false"
+                user_race.dexterity = user_race.dexterity + bonus
+            elif choice == 3 :
+                bonus_user_input = "false"
+                user_race.wisdom = user_race.wisdom + bonus
+            elif choice == 4 :
+                bonus_user_input = "false"
+                user_race.intelligence = user_race.intelligence + bonus
+            else:
+                print ('\nYou selected something invalid\n')
+            # the string below clears the screen
+            os.system ('cls')
+    elif choice == '5' :
+        valid_user_input = "true"
+        user_race = Ancestry.Goblin (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus)
+        bonus_user_input = "true"
+        while bonus_user_input == "true":
+            print ('\nWhat is your bonus stat?\n')
+            print ('1) Str')
+            print ('2) Con')
+            print ('3) Wis')
+            print ('4) Int')
+            choice = int(input(''))
+            print (choice)
+            bonus = 2
+            if choice == 1 :
+                bonus_user_input = "false"
+                user_race.strength = user_race.strength + bonus
+            elif choice == 2 :
+                bonus_user_input = "false"
+                user_race.constitution = user_race.constitution + bonus
+            elif choice == 3 :
+                bonus_user_input = "false"
+                user_race.wisdom = user_race.wisdom + bonus
+            elif choice == 4 :
+                bonus_user_input = "false"
+                user_race.intelligence = user_race.intelligence + bonus
+            else:
+                print ('\nYou selected something invalid\n')
+            # the string below clears the screen
+            os.system ('cls')
+    elif choice == '6' :
+        valid_user_input = "true"
+        user_race = Ancestry.Human (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus)
+        bonus_user_input = 0
+        while bonus_user_input <= 1:
+            print ('\nWhat is your bonus stat?\n')
+            print ('1) Str')
+            print ('2) Dex')
+            print ('3) Con')
+            print ('4) Int')
+            print ('5) Wis')
+            print ('6) Cha')
+            choice = int(input(''))
+            print (choice)
+            bonus = 2
+            if choice == 1 :
+                bonus_user_input = bonus_user_input + 1
+                if bonus_user_input == 1 :
+                    print ('you cant select the same bonus')
+                else :
+                    user_race.strength = user_race.strength + bonus
+            elif choice == 2 :
+                bonus_user_input = bonus_user_input + 1
+                if bonus_user_input == 1 :
+                    print ('you cant select the same bonus')
+                else :
+                    user_race.dexterity = user_race.dexterity + bonus
+            elif choice == 3 :
+                bonus_user_input = bonus_user_input + 1
+                if bonus_user_input == 1 :
+                    print ('you cant select the same bonus')
+                else :
+                    user_race.constitution = user_race.constitution + bonus
+            elif choice == 4 :
+                bonus_user_input = bonus_user_input + 1
+                if bonus_user_input == 1 :
+                    print ('you cant select the same bonus')
+                else :
+                    user_race.intelligence = user_race.intelligence + bonus
+            elif choice == 5 :
+                bonus_user_input = bonus_user_input + 1
+                if bonus_user_input == 1 :
+                    print ('you cant select the same bonus')
+                else :
+                    user_race.wisdom = user_race.wisdom + bonus
+            elif choice == 6 :
+                bonus_user_input = bonus_user_input + 1
+                if bonus_user_input == 1 :
+                    print ('you cant select the same bonus')
+                else :
+                    user_race.charisma = user_race.charisma + bonus
+            else:
+                print ('\nYou selected something invalid\n')
+            # the string below clears the screen
+            os.system ('cls')
+    elif choice == '3' :
+        valid_user_input = "true"
+        user_race = Ancestry.Elf (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus)
+        bonus_user_input = "true"
+        while bonus_user_input == "true":
+            print ('\nWhat is your bonus stat?\n')
+            print ('1) Str')
+            print ('2) Con')
+            print ('3) Wis')
+            print ('4) Cha')
+            choice = int(input(''))
+            print (choice)
+            bonus = 2
+            if choice == 1 :
+                bonus_user_input = "false"
+                user_race.strength = user_race.strength + bonus
+            elif choice == 2 :
+                bonus_user_input = "false"
+                user_race.constitution = user_race.constitution + bonus
+            elif choice == 3 :
+                bonus_user_input = "false"
+                user_race.wisdom = user_race.wisdom + bonus
+            elif choice == 4 :
+                bonus_user_input = "false"
+                user_race.charisma = user_race.charisma + bonus
+            else:
+                print ('\nYou selected something invalid\n')
+            # the string below clears the screen
+            os.system ('cls')
+    elif choice == '3' :
+        valid_user_input = "true"
+        user_race = Ancestry.Elf (strength, dexterity, constitution, intelligence, wisdom, charisma, health, speed, heritage_bonus)
+        bonus_user_input = "true"
+        while bonus_user_input == "true":
+            print ('\nWhat is your bonus stat?\n')
+            print ('1) Str')
+            print ('2) Con')
+            print ('3) Wis')
+            print ('4) Cha')
+            choice = int(input(''))
+            print (choice)
+            bonus = 2
+            if choice == 1 :
+                bonus_user_input = "false"
+                user_race.strength = user_race.strength + bonus
+            elif choice == 2 :
+                bonus_user_input = "false"
+                user_race.constitution = user_race.constitution + bonus
+            elif choice == 3 :
+                bonus_user_input = "false"
+                user_race.wisdom = user_race.wisdom + bonus
             elif choice == 4 :
                 bonus_user_input = "false"
                 user_race.charisma = user_race.charisma + bonus
