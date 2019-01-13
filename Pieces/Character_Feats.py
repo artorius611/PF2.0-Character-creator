4086f5e690e6b1a72ada37becb59f871796586ec
alexander weiss <42820958+artorius611@users.noreply.github.com>
2019-01-12 22:42:45 -0600

    Create Character_Feats.py

---
+++ Pieces/Character_Feats.py
@@ -0,0 +1,31 @@
+class Character_feats :
+    def __init__ (self) :
+        self.ancestry_feats = {
+            1 : "false",
+            5 : "false",
+            9 : "false",
+            13 : "false",
+            17 : "false"
+        }
+
+        self.skill_feats = {
+            'Background' : "false",
+            2 : "false",
+            4 : "false",
+            6 : "false",
+            8 : "false",
+            10 : "false",
+            12 : "false",
+            14 : "false",
+            16 : "false",
+            18 : "false",
+            20 : "false"
+        }
+
+        self.general_feats = {
+            3 : "false",
+            7 : "false",
+            11 : "false",
+            15 : "false",
+            19 : "false"
+        }
