2053fa908768d0cb3bf200257f5bcbeeafe8d04b
alexander weiss <42820958+artorius611@users.noreply.github.com>
2019-01-19 13:31:15 -0600

    Create Race.py

---
+++ Pieces/Race.py
@@ -0,0 +1,12 @@
+
+class Selection() :
+    def __init__ (self, choice) :
+        print ('\nWhat is your Ancestry?\n')
+        print ('1) Dwarf')
+        print ('2) Elf')
+        print ('3) Gnome')
+        print ('4) Goblin')
+        print ('5) Halfling')
+        print ('6) Human')
+        self.choice = input('Which Ancestry do you choose? ')
+        
